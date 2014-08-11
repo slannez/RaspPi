a15f2605e6027bd3bf8d2200eb652529e66bfb1e
Sebastien Lannez <sebastien.lannez@gmail.com>
2014-08-11 18:49:24 +0200

    json output

--- 007f0101/src/telemeter.c
+++ 007f0101/src/telemeter.c
@@ -1,9 +1,16 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <errno.h>
 #include <termios.h>
+#include <sys/time.h>
+
+/**
+ * Hypothesis: No more than 255 characters received from
+ *             teleinfo per message
+ **/
 
 int open_port(void) {
   int fd;
@@ -25,14 +32,10 @@ int configure(int fd) {
   options.c_cflag &= ~CSIZE;
   options.c_cflag |= CS7;
   options.c_cflag &= ~(CRTSCTS);
-
   options.c_iflag |= (INPCK | ISTRIP);
   options.c_iflag &= ~(IXON | IXOFF | IXANY | ICRNL);
-  
   options.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);
-
   options.c_oflag &= ~OPOST;
-
   options.c_cc[VTIME] = 80;
   options.c_cc[VMIN] = 0;
 
@@ -58,6 +61,8 @@ int main(int iargc, const char** arg){
   char *bufptr;
   int nbytes;
   char ch[3];
+  struct timeval tv;
+  int irec;
   
   fd = open_port();
   configure(fd);
@@ -65,6 +70,7 @@ int main(int iargc, const char** arg){
   while (1) {
     tcflush(fd, TCIFLUSH);
     ch[0] = 0;
+    /* Skip message separation */
     do {
       ch[3] = ch[0];
       nbytes = read(fd, ch, 1);
@@ -72,15 +78,26 @@ int main(int iargc, const char** arg){
     } while( (ch[0] != 0x02) || (ch[3] != 0x03) );
 
     bufptr = buffer;
+    (*bufptr++) = '{';
+    irec = 0;
     do {
       nbytes = read(fd, ch, 1);
       if (nbytes==0) { close(fd); exit(2); }
-      (*bufptr)=ch[0];
-      ++bufptr;
+      if (irec<=1) {
+        /* DATA */
+        if (ch[0]==' ') { ++irec; if (irec==1) { (*bufptr++)=':'; } }
+        else if (ch[0]=='\n' || ch[0]=='\r') { }
+        else { (*bufptr++) = ch[0]; }
+      } else {
+        /* CRC */
+        if (ch[0]=='\r') { (*bufptr++)=','; irec=0; }
+        else if (ch[0]=='\n') { irec=0; }
+      }
     } while(ch[0] != 0x03);
-    *bufptr = 0;
+    (*bufptr) = 0;
+    gettimeofday(&tv,NULL);
+    sprintf(bufptr,"ts:{sec:%i,usec:%i}}",tv.tv_sec,tv.tv_usec);
     printf("%s\n",buffer);
-    printf("-----------\n");
   }
   close(fd);
   return(0);
